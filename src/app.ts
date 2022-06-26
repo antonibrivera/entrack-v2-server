4602351ba2c9b0e90b703a6f03435fa312bd9da2
Joseph Rivera <antonibrivera@gmail.com>
2022-06-26 17:47:21 -0400

    installed 'express' and set up '/src' directory with skeleton files and structure.

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+
+
+const app = express()
+const { NODE_ENV } = require('./config')
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+// insert "app.use('/route', fileName)" when creating individual files for the routes and their actions.
+
+app.use(function errorHandling(err, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production')  {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(err)
+    response = { message: err.message, err }
+  }
+  res.status(500).json(response)
+})
+
+
+module.exports = app
